5f7cd295ce0c849c66c85229a00cb45184d79822
AndreaTalarico96 <78956185+AndreaTalarico96@users.noreply.github.com>
2021-02-18 15:30:07 +0100

    homepage

--- js/imgInjectScript.js
+++ js/imgInjectScript.js
@@ -29,7 +29,7 @@ function CreateAndAppendServix(place, array){
         //CREAZIONE DELL'ELEMENTO
         let element = document.createElement("div");
         element.innerHTML = 
-       `<img class="d-block carolImage" src="resources\\${set.image}" alt="Image rended improperly">`;
+       `<img class="d-block carolImage" src="resources/${set.image}" alt="Image rended improperly">`;
         //console.log(element); 
         element.classList.add("carousel-item")
         element.classList.add("carolImageContain")
@@ -39,8 +39,6 @@ function CreateAndAppendServix(place, array){
     }
 
 }
-
-function createAndAppendImgCard()
 function SetActiveClass(place){
     let element = place.firstElementChild;
     console.log(element);
