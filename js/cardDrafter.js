59995fe632f60948ba978faf964f7749318a3db4
AndreaTalarico96 <78956185+AndreaTalarico96@users.noreply.github.com>
2021-02-20 12:05:06 +0100

    Update cardDrafter.js

--- js/cardDrafter.js
+++ js/cardDrafter.js
@@ -1,26 +1,20 @@
-var ajaxRequest = new XMLHttpRequest();
+var ajaxRequest2 = new XMLHttpRequest();
 
-	ajaxRequest.onreadystatechange = function(){
+	ajaxRequest2.onreadystatechange = function(){
 	
-		if(ajaxRequest.readyState == 4){
+		if(ajaxRequest2.readyState == 4){
 			//the request is completed, now check its status
-			if(ajaxRequest.status == 200){
+			if(ajaxRequest2.status == 200){
 				//turn JSON into object
 				var jsonObj = JSON.parse(ajaxRequest.responseText);
 
-				const place = document.getElementById("cardsBody");
 				const galleryPlace = document.getElementById("galleryPlace");
-				const pageIndex =place.dataset.currentfile;
+
 				for (let i = 0; i < jsonObj.imgSet.length; i++){
-					if (pageIndex == jsonObj.imgSet[i].categoria)
+					if (jsonObj.imgSet[i].categoria == 1)
 					{
-					//Build(jsonObj.imgSet[i], place);
 					BuildGalleryDiv(jsonObj.imgSet[i], galleryPlace, i);
-					//CreateAndAppend(jsonObj.imgSet[i], place);
 					}
-				}
-				if (place.innerHTML=""){
-					place.innerHtml="Nulla da vedere";
 				}
 			}else{
 				console.log("Status error: " + ajaxRequest.status);
@@ -32,8 +26,8 @@ var ajaxRequest = new XMLHttpRequest();
 	
 	}
 	
-	ajaxRequest.open('GET', 'https://clayplayground.github.io/resources/imageSet.json');
-	ajaxRequest.send();
+	ajaxRequest2.open('GET', 'https://clayplayground.github.io/resources/imageSet.json');
+	ajaxRequest2.send();
 
 	
 
@@ -56,34 +50,6 @@ var ajaxRequest = new XMLHttpRequest();
 		place.appendChild(element);
 	}
 	
-	function Build(objSet, place){
-		let elem = document.createElement("div");
-		elem.innerHTML = `
-			<img class="card-img-top cardImg btnAble" src="resources/${objSet.nomeFile}" alt="${objSet.titolo}">
-			<div class="card-body">
-				<h2 class="cardTitle">${objSet.titolo}
-				</h2>
-				<p class="card-text">${objSet.BreveDescrizione}</p>
-			</div>`;
-		elem.classList.add("card");
-		elem.classList.add("cardCustom");
-		elem.style("width", "18rem");
-		
-		document.getElementById("cardsBody").appendChild(elem);
+	
 
-		place.appendChild(elem);
-	}
-
-	function CreateAndAppend(place, obj){
-
-        let element = document.createElement("div");
-            element.innerHTML = 
-           `<img class="d-block carolImage" src="resources/${obj.nomeFile}" alt="${obj.BreveDescrizione}">`;
-            //console.log(element); 
-            element.classList.add("carousel-item")
-            element.classList.add("carolImageContain")
-    
-            //APPENDERE L'ELEMENTO
-            place.appendChild(element);
-    
-    }
+	
